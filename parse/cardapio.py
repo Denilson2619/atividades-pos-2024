15e0fd3b4b60b85b3e12ad11395ed65e2ec6395f
Denilson2619 <p.denilson@escolar.ifrn.edu.br>
2024-08-13 23:54:13 -0300

    parse cardapio CERTO

--- parse/cardapio.py
+++ parse/cardapio.py
@@ -1,6 +1,6 @@
 from xml.dom.minidom import parse
 
-# Caminho para o arquivo de teste
+
 file_path = "C:/Users/patom/OneDrive/Desktop/Repositório/atividades-pos-2024/parse/cardapio.xml"
 
 try:
@@ -18,7 +18,7 @@ for prato in pratos_cardapio:
     print(f"Prato {prato_id_cardapio}: {nome}")
 
 try:
-    prato_id = int(input("DIGITE O ID DO PRATO/CARDAPIO: ")) - 1  # Ajuste para índice zero baseado
+    prato_id = int(input("DIGITE O ID DO PRATO/CARDAPIO: ")) - 1 
     prato = pratos_cardapio[prato_id]
 except (ValueError, IndexError):
     print("ID do prato inválido.")
